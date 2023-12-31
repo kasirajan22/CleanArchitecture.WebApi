bdf8dfda94acac38891ea05d0d7a4662d7271aab
magizhan <kasirajan22@hotmail.com>
2023-12-31 19:12:43 +0530

    Added Docker support

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
     }
-    [Authorize]
+    // [Authorize]
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
