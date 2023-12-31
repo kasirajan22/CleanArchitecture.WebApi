7ceb1712cdfd7eeed88648f16d6a88ee600f86e9
magizhan <kasirajan22@hotmail.com>
2023-12-31 17:36:06 +0530

    Revert "Added ProgramExt, SwaggerExt & Jwt"
    This reverts commit 560844fc5627ebb2fe4058056231ee706f403ae9.

---
+++ WebAPI/Program.cs
@@ -0,0 +1,30 @@
+using WebAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+var configBuilder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureMSSqlContext(configBuilder);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
